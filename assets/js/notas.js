bdbd9742437fbf8c8e83a2eca05c1e23474e584b
Andre Leandro <andrephilleandro@gmail.com>
2024-07-22 19:35:07 -0300

    edit note

--- assets/js/notas.js
+++ assets/js/notas.js
@@ -5,6 +5,28 @@ function abremodal() {
 function fechamodal() {
     document.getElementById("modal").style.display = "none";
 }
+
+
+function editNote(index) {
+    const notes = JSON.parse(localStorage.getItem('notas')) || [];
+    const note = notes[index];
+    
+    if (note) {
+        const newTitle = prompt('Novo título:', note.titulo);
+        const newDescription = prompt('Nova descrição:', note.descricao);
+        
+        note.titulo = newTitle;
+        note.descricao = newDescription;
+        
+        localStorage.setItem('notas', JSON.stringify(notes));
+        exibirNotas();
+    } else {
+        alert('Nota não encontrada!');
+    }
+}
+
+
+
 // Localstorage 1
 document.getElementById("FormNotas").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -41,7 +63,7 @@ function exibirNotas() {
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                             </svg>
                         </button>
-                        <button type="button" class="btn btn-outline-info" onclick="editarNota(${index})">
+                        <button type="button" class="btn btn-outline-info" onclick="editNote(${index})">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                             </svg>
@@ -74,6 +96,5 @@ function notaConclu(index) {
     localStorage.setItem("notas", JSON.stringify(notas));
     exibirNotas();
 }
-
 
 
